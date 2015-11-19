7e7bdda7eda34545a09e3ae069f2b04c4439adfd
Ferose Babu <ferose2@gmail.com>
2015-11-19 13:50:18 -0500

    Init commit

--- CardReader/ViewController.m
+++ CardReader/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import "CardIO.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource, CardIOViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIView *cardView;
+
+@property (weak, nonatomic) CardIOView *cardIOView;
+@property (nonatomic) NSMutableArray *cards;
 
 @end
 
@@ -16,12 +23,70 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    if (![CardIOUtilities canReadCardWithCamera]) {
+        return;
+    }
+    
+    self.cards = [NSMutableArray array];
+    
+    [self resetCardIOView];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [CardIOUtilities preload];
+}
+
+- (void)resetCardIOView
+{
+    [self.cardIOView removeFromSuperview];
+    
+    CardIOView *cardIOView = [[CardIOView alloc] initWithFrame:self.cardView.bounds];
+    cardIOView.allowFreelyRotatingCardGuide = NO;
+    cardIOView.delegate = self;
+    [self.cardView addSubview:cardIOView];
+    self.cardIOView = cardIOView;
+    
+    NSDictionary *views = @{@"cardView": self.cardIOView};
+    [self.cardView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[cardView]|" options:0 metrics:nil views:views]];
+    [self.cardView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[cardView]|" options:0 metrics:nil views:views]];
+    self.cardIOView.translatesAutoresizingMaskIntoConstraints = NO;
+}
+
+- (void)cardIOView:(CardIOView *)cardIOView didScanCard:(CardIOCreditCardInfo *)cardInfo
+{
+    [self resetCardIOView];
+    [self.cards addObject:cardInfo];
+    [self.tableView reloadData];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.cards.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"CardCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    CardIOCreditCardInfo *cardInfo = self.cards[indexPath.row];
+    cell.textLabel.text = cardInfo.cardNumber;
+    
+    return cell;
+}
+
+- (IBAction)shareTapped:(id)sender {
+    
+    UIActivityViewController *activityController = [[UIActivityViewController alloc] initWithActivityItems:@[[[self.cards valueForKeyPath:@"cardNumber"] componentsJoinedByString:@"\n"]] applicationActivities:nil];
+    [self presentViewController:activityController animated:YES completion:nil];
 }
 
 @end
