be27dc4f15b419d4c4364e2a64dc8ca6efb4a27f
Ferose Babu <ferose2@gmail.com>
2015-11-19 12:04:56 -0500

    Initial Commit

---
+++ CardReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardReader
+//
+//  Created by Ferose Babu on 11/19/15.
+//  Copyright © 2015 Ferose Babu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
